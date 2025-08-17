d872a24a1bb0184e11f9cd2380b0c099b1fba3b7
ALEXSTONE <solo-drive@bk.ru>
2025-08-17 11:05:30 +0300

    lesson_69 - созданы восстановление пароля

--- users/views.py
+++ users/views.py
@@ -1,10 +1,81 @@
 from django.shortcuts import redirect
-from .forms import CustomRegisterForm, CustomLoginForm, CustomPasswordChangeForm
+from .forms import (
+    CustomRegisterForm,
+    CustomLoginForm,
+    CustomPasswordChangeForm,
+    CustomSetPasswordForm,
+    CustomPasswordResetForm,
+)
 from django.views.generic.edit import CreateView
-from django.contrib.auth.views import LogoutView, LoginView, PasswordChangeView
+from django.contrib.auth.views import (
+    LogoutView,
+    LoginView,
+    PasswordChangeView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.contrib import messages
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+
+
+class CustomPasswordResetView(PasswordResetView):
+    """
+    1. Начало сброса пароля. Человек вводит емейл для сброса
+    """
+
+    template_name = "users_login_registr.html"
+    form_class = CustomPasswordResetForm
+    success_url = reverse_lazy("password_reset_done")
+    email_template_name = "password_reset_email.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["operation_type"] = "Восстановление пароля"
+        return context
+
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    """
+    2. Загляните в Emeil - вам ушло письмо с инструкциями
+    """
+
+    message = "Инструкции по восстановлению пароля отправлены на Email"
+    operation_type = "Внимание!"
+
+    template_name = "users_message.html"
+    extra_context = {
+        "operation_type": operation_type,
+        "message": message,
+    }
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    """
+    3. Вью где отображается форма восстановление пароля (можно попасть только через платформу 9 3\4
+    ) - комбо <uidb64> + <token>
+    """
+
+    operation_type = "Сменить пароль"
+    extra_context = {"operation_type": operation_type}
+    form_class = CustomSetPasswordForm
+    template_name = "users_login_registr.html"
+    success_url = reverse_lazy("password_reset_complete")
+
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    """
+    Ура! Вы сбросили пароль!
+    """
+    message = "Вы успешно сменили пароль!"
+    operation_type = "Внимание!"
+    extra_context = {
+        "operation_type": operation_type,
+        "message": message,
+    }
+    template_name = "users_message.html"
 
 
 class CustomPasswordChangeView(PasswordChangeView):
@@ -15,11 +86,12 @@ class CustomPasswordChangeView(PasswordChangeView):
     def form_valid(self, form):
         messages.success(self.request, "Пароль успешно изменен!")
         return super().form_valid(form)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["operation_type"] = "Смена пароля"
         return context
+
 
 class CustomRegisterView(CreateView):
     form_class = CustomRegisterForm
